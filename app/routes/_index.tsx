d5b2c768f9f03cb8df622b20554126e58d440f68
Soleone <dev@soleone.xyz>
2023-09-24 19:15:26 -0400

    Fix typing for loader function and useLoaderData

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,12 +2,15 @@ import { json, type LoaderFunction } from "@vercel/remix"
 import { Card, CardDescription, CardHeader, CardTitle } from "~/components/ui/card"
 import { db } from "~/lib/db.server"
 
-export const loader: LoaderFunction = async () => {
-  // const myModels = await db.modelname.findMany()
+export async function loader() {
+  // const list = await db.modelname.findMany()
+  //return json({ list })
   return json({ success: true })
 }
 
 export default function Index() {
+  //const { list } = useLoaderData<typeof loader>()
+
   return <Card>
     <CardHeader>
       <CardTitle>Component Test</CardTitle>
